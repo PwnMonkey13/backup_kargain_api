634ba30060bbc5d09069d8eb9ff95cb75644d3c1
Nicolas Giraudo <giraudo.nicolas13@gmail.com>
2020-03-06 00:39:33 +0100

    CORS

--- src/config/server.js
+++ src/config/server.js
@@ -1,62 +1,50 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser');
-const session = require('cookie-session');
+const cookieParser = require('cookie-parser')
 const helmet = require('helmet')
 const cors = require('cors')
 const passport = require('passport')
 const config = require('./config')
 const routes = require('../routes')
 const app = express()
-const tableRoutes = require('../routes/tableRoutes');
+const tableRoutes = require('../routes/tableRoutes')
 
+app.use(helmet())
 app.use(bodyParser.json())
-app.use(cookieParser());
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
-app.use(passport.initialize({session : false }));
+app.use(passport.initialize({ session: false }))
 
-// app.use(session({
-//   secret: 'somesecret',
-//   key: 'sid',
-//   cookie: {
-//     secure: true,
-//     maxAge: 5184000000 // 2 months
-//   }
-// }));
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+  credentials: true,
+  preflightContinue: true,
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
-app.use((req, res, next) => {
-  console.log(req.headers.origin);
-
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE')
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next()
-})
+app.use(cors(corsOptions))
+//   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE')
 
 app.use('/api', routes)
 
-
 app.get('/', function (req, res, next) {
   res.json({
-      success: true,
-      msg : `API is serving from ${config.db.mongo_location}`
-    });
+    success: true,
+    msg: `API is serving from ${config.db.mongo_location}`
+  })
 })
 
-app.get('/endpoints', function(req, res, next){
-  tableRoutes(app);
-});
-
-// Tells Express to allows data to be sent across different origins
-// required by CORS policy
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "localhost:3000");
-//   res.header('Access-Control-Allow-Credentials', true);
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.get('/endpoints', function (req, res, next) {
+  tableRoutes(app)
+})
 
 app.get('*', function (req, res, next) {
   const err = new Error('Page Not Found')
@@ -65,16 +53,11 @@ app.get('*', function (req, res, next) {
 })
 
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = config.isDev ? err : {};
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-
   let msg = err.message || err
-  msg = config.isDev ? msg : 'Something failed on server';
-  res.json({ success: false, msg});
+  msg = config.isDev ? msg : 'Something failed on server'
+  console.log(msg)
+  res.status(err.status || 500).json({ success: false, msg })
 })
 
 module.exports = app
