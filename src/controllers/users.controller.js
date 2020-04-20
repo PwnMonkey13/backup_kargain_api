d184157eab1d0145e64bf75f09b4591141acc3bc
Nicolas Giraudo <giraudo.nicolas13@gmail.com>
2020-04-20 19:38:55 +0200

    users sorting

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,63 +1,61 @@
-const User = require('../models').User;
-const functions = require('../utils/functions');
+const User = require('../models').User
+const functions = require('../utils/functions')
 
 const getUsers = async (req, res, next) => {
+
   try {
     const page = (req.query.page && parseInt(req.query.page) > 0) ? parseInt(req.query.page) : 1
     const sort = (req.query.sort) ? { [req.query.sort]: 1 } : {}
     let size = 10
+
     if (req.query.size && parseInt(req.query.size) > 0 && parseInt(req.query.size) < 500) {
       size = parseInt(req.query.size)
     }
+
     const skip = (size * (page - 1) > 0) ? size * (page - 1) : 0
+    const rows = await User.find().skip(skip).sort(sort).limit(size)
+    const total = await User.estimatedDocumentCount().exec()
 
     const response = {
-      users: [],
       page: page,
+      pages: Math.ceil(total / size),
+      total,
       size: size,
       sort: req.params.sort,
-      total: 0
+      rows
     }
 
-    const users = await User
-      .find()
-      .skip(skip)
-      .sort(sort)
-      .limit(size)
-
-    response.users = users
-    response.total = users.length
-    return res.json(response)
-  }catch (e) {
-    next(e);
+    return res.json({ success: true, data: response })
+  } catch (e) {
+    next(e)
   }
 }
 
 const getUser = async (req, res, next) => {
   try {
-    const username = req.params.username;
+    const username = req.params.username
     const user = await User.findOne({ username })
-    if (!user) return next('No user found');
-    else res.json({ success: true, data : user });
+    if (!user) return next('No user found')
+    else res.json({ success: true, data: user })
   } catch (err) {
     next(err)
   }
 }
 
 const updateUser = (req, res, next) => {
-  const filter = { username: req.params.username };
-  const update = functions.convertToDotNotation(req.body);
-  User.findOneAndUpdate(filter,update, { new: true })
+  const filter = { username: req.params.username }
+  const update = functions.convertToDotNotation(req.body)
+  User.findOneAndUpdate(filter, update, { new: true })
     .then(updated => {
-      return res.status(200).json({ success: true, data: updated });
-    }).catch(next);
+      return res.status(200).json({ success: true, data: updated })
+    }).catch(next)
 }
 
 const deleteUser = async (req, res, next) => {
-    const uid = req.params.uid;
-    User.deleteOne({ _id: uid }).then(document => {
-      return res.json({ success: true, data: document })
-    }).catch(next);
+  const uid = req.params.uid
+  User.deleteOne({ _id: uid }).then(document => {
+    return res.json({ success: true, data: document })
+  }).catch(next)
 }
 
 module.exports = { getUsers, getUser, updateUser, deleteUser }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -11,12 +11,13 @@ router.get('/',
   usersController.getUsers);
 // users?page=${page}&size=${size}
 
+router.get('/:username', usersController.getUser)
+
 router.put('/:username',
   passportAuth.authenticate('jwt', { session: false }),
   // auth.requireAdminOrSelf,
   usersController.updateUser);
 
-router.get('/:username', usersController.getUser)
 
 router.delete('/:uid', usersController.deleteUser)
 
