3b5c785e41ed81a9fb60c497c818398b724ab68d
Nicolas Giraudo <giraudo.nicolas13@gmail.com>
2020-05-04 04:00:00 +0200

    ops

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpackNodeExternals = require('webpack-node-externals');
 
 module.exports = {
   mode: 'production',
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'api.bundle.js'
+    filename: 'index.js'
   },
+  module: {
+    rules: [{ test: /\.js$/, use: 'babel-loader' }],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+  externals: [webpackNodeExternals()],
   target: 'node'
-};
+}
